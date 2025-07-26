4faf67e95e2dda49553dde2b59b8694e6eb4b30a
DongJoo <looloo8910@gmail.com>
2025-07-26 10:35:03 +0900

    자동미분

--- Test.py
+++ Test.py
@@ -1,92 +1,29 @@
 import torch
 
-#텐서 조작
+#자동미분
+a = torch.randn(3, 3)  # 3x3 랜덤 텐서 생성
+a = a * 3              # 값들을 3배로 스케일링
+print(a)               # 텐서 출력
+print(a.requires_grad) # 현재 gradient 추적 여부 (False)
 
-x = torch.Tensor([[1,2],[3,4]])
+a.requires_grad_(True) # 이제부터 a에 대해 gradient 추적 활성화
+print(a.requires_grad) # True 출력
+
+b = (a * a).sum()      # b는 a^2 의 총합 (스칼라)
+print(b)               # b 값 출력
+print(b.grad_fn)       # b가 어떤 연산으로 만들어졌는지 추적함
+print()
+
+#기울기
+x=torch.ones(3,3,requires_grad=True)
 print(x)
-
-print(x[0,0])
-print(x[0,1])
-print(x[1,0])
-print(x[1,1])
-
-#슬라이싱
-print(x[:,0])
-print(x[:,1])
-print(x[0,:])
-print(x[1,:])
-
-x=torch.randn(4,5)
-print(x)
-y= x.view(20)
+y = x + 5
 print(y)
+
+z=y*y
+out = z.mean()
+print(z,out)
+
+print(out)
+out.backward()
 print()
-
-#x.item() 값이 하나만 있을 때에 값을 가져옴
-tensor = torch.rand(1,3,3)
-print(tensor)
-print(tensor.shape)
-
-t = tensor.squeeze()
-print(t)
-print(t.shape)
-
-t.unsqueeze_(2)
-print(t)
-print(t.shape)
-print()
-
-#stack
-x= torch.FloatTensor([1,4])
-print(x)
-y= torch.FloatTensor([2,5])
-print(y)
-z= torch.FloatTensor([3,6])
-print(z)
-print(torch.stack([x,y,z]))
-print()
-
-#cat
-a= torch.randn(1,3,3)
-b= torch.randn(1,3,3)
-c= torch.cat((a,b),dim=1)
-print(a)
-print(b)
-print(c.size())
-
-#chunk
-tensor = torch.rand(3,6)
-print(tensor)
-t1,t2,t3=torch.chunk(tensor,3,dim=1)
-print(t1)
-print(t2)
-print(t3)
-print()
-
-#split
-tensor = torch.rand(3,6)
-print(tensor)
-t1,t2,t3=torch.chunk(tensor,3,dim=1)
-print(t1)
-print(t2)
-print(t3)
-
-#numpy
-
-a = torch.ones(7)
-print(a)
-b = a.numpy()
-print(b)
-
-a.add_(1)
-print(a)
-print(b)
-print()
-
-import numpy as np
-
-a = np.ones(7)
-b = torch.from_numpy(a)
-np.add(a,1,out=a)
-print(a)
-print(b)
