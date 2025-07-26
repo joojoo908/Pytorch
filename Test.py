60ffcb09982c5d2c40e726b01b8fa72ecacd5f3e
DongJoo <looloo8910@gmail.com>
2025-07-26 14:42:58 +0900

    레이어

--- Test.py
+++ Test.py
@@ -1,37 +1,27 @@
 import torch
 
 import torch.nn as nn
+import torch.nn.functional as F
 
 #신경망 구성
 
 def main():
-    #nn.Linear 계층
-    input = torch.randn(128,20)
-    print(input)
-
-    m=nn.Linear(20,30)
-    print(m)
-
-    output = m(input)
-    print(output)
-    print(output.size())
-    print()
-
-    #nn.conv2d 계층
-    input = torch.randn(20,16,50,100)
-    print(input.size())
-    m=nn.Conv2d(16,33,3,stride=2)
-    m=nn.Conv2d(16,33,(3,5),stride=(2,1),
-                padding=(4,2))
-    m = nn.Conv2d(16, 33, (3, 5), stride=(2, 1),
-                  padding=(4, 2), dilation=(3,1))
-    print(m)
-    output = m(input)
-    #print(output)
-    print(output.size())
-    print()
-
     #컨볼루션 레이어
+    nn.Conv2d(in_channels=1,out_channels=20,kernel_size=5,stride=1)
+    layer = nn.Conv2d(1,20,5,1).to(torch.device('cuda:0'))
+    print(layer)
+
+    weight = layer.weight
+    print(weight.shape)
+    print()
+
+    #풀링 레이어
+    #pool = F.max_pool2d(output,2,2)
+
+    #선형 레이어
+    # 1d 로 펼쳐줌 
+
+
 
 if __name__ == '__main__':
     main()
