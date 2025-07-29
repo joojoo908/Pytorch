4f7b87013f9662a881d38f0972c1cee820b33bdb
DongJoo <looloo8910@gmail.com>
2025-07-29 17:22:39 +0900

    매트릭, 학습결과 확인용

--- Test.py
+++ Test.py
@@ -2,52 +2,28 @@ import torch
 
 import torch.nn as nn
 import torch.nn.functional as F
+import torchmetrics
 
-#신경망 구성(레이어로 이루어짐)
-#모델 정의
+#매트릭 (모니터링용 지표)
 
-# class Model(nn.Module):
-#     def __init__(self,inputs):
-#         super(Model, self).__init__()
-#         self.layer = nn.Linear(inputs,1)
-#         self.activation = nn.Sigmoid()
-#
-#     def forward(self, x):
-#         x = self.layer(x)
-#         x = self.activation(x)
-#         return(x)
+preds = torch.randn(10, 5).softmax(dim=-1)
+target = torch.randint(5,(10, ))
+print(preds,target)
 
-# model = Model(1)
-# print(list(model.children()))
-# print(list(model.modules()))
+acc=torchmetrics.functional.accuracy(preds, target,task="multiclass", num_classes=5)
+print(acc)
+print()
 
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2)
-        )
 
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=30, kernel_size=5),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(2)
-        )
+metric = torchmetrics.Accuracy( task="multiclass", num_classes=5)
 
-        self.layer3 = nn.Sequential(
-            nn.Linear(in_features=30*5*5, out_features=10, bias=True),
-            nn.ReLU(inplace=True)
-        )
+n_batches =10
+for i in range(n_batches):
+    preds = torch.randn(10, 5).softmax(dim=-1)
+    target = torch.randint(5, (10,))
 
-    def forward(self, x):
-        x=self.layer1(x)
-        x=self.layer2(x)
-        x=x.view(x.shape[0], -1)
-        x=self.layer3(x)
-        return x
+    acc = metric(preds, target)
+    print(acc)
 
-model = Model()
-print(list(model.children()))
-print(list(model.modules()))
+ac=metric.compute()
+print('\n',acc)
