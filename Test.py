d66d5a1d3037d306d250a386c40aa49289e283d0
DongJoo <looloo8910@gmail.com>
2025-07-29 17:02:50 +0900

    모델 정의하는법

--- Test.py
+++ Test.py
@@ -3,25 +3,51 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-#신경망 구성
+#신경망 구성(레이어로 이루어짐)
+#모델 정의
 
-def main():
-    #컨볼루션 레이어
-    nn.Conv2d(in_channels=1,out_channels=20,kernel_size=5,stride=1)
-    layer = nn.Conv2d(1,20,5,1).to(torch.device('cuda:0'))
-    print(layer)
+# class Model(nn.Module):
+#     def __init__(self,inputs):
+#         super(Model, self).__init__()
+#         self.layer = nn.Linear(inputs,1)
+#         self.activation = nn.Sigmoid()
+#
+#     def forward(self, x):
+#         x = self.layer(x)
+#         x = self.activation(x)
+#         return(x)
 
-    weight = layer.weight
-    print(weight.shape)
-    print()
+# model = Model(1)
+# print(list(model.children()))
+# print(list(model.modules()))
 
-    #풀링 레이어
-    #pool = F.max_pool2d(output,2,2)
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
 
-    #선형 레이어
-    # 1d 로 펼쳐줌 
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=30, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
 
+        self.layer3 = nn.Sequential(
+            nn.Linear(in_features=30*5*5, out_features=10, bias=True),
+            nn.ReLU(inplace=True)
+        )
 
+    def forward(self, x):
+        x=self.layer1(x)
+        x=self.layer2(x)
+        x=x.view(x.shape[0], -1)
+        x=self.layer3(x)
+        return x
 
-if __name__ == '__main__':
-    main()
+model = Model()
+print(list(model.children()))
+print(list(model.modules()))
