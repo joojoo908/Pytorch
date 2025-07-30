342dc93f1bdebe42a83a59227c96c27d641312bc
DongJoo <looloo8910@gmail.com>
2025-07-30 17:26:15 +0900

    모델학습

--- Test.py
+++ Test.py
@@ -1,29 +1,67 @@
 import torch
 
+import numpy as np
 import torch.nn as nn
+import torch.optim as optim
 import torch.nn.functional as F
 import torchmetrics
 
-#매트릭 (모니터링용 지표)
+import matplotlib.pyplot as plt
 
-preds = torch.randn(10, 5).softmax(dim=-1)
-target = torch.randint(5,(10, ))
-print(preds,target)
+#데이터셋
+x=torch.randn(200,1)*10
+y=x+3*torch.randn(200,1)
+plt.scatter(x.numpy(),y.numpy())
+plt.ylabel('y')
+plt.xlabel('x')
+plt.grid()
+plt.show()
 
-acc=torchmetrics.functional.accuracy(preds, target,task="multiclass", num_classes=5)
-print(acc)
-print()
+#심플 모델
+class LinearRegressionModel(nn.Module):
+    def __init__(self):
+        super(LinearRegressionModel, self).__init__()
+        self.linear = nn.Linear(1,1)
 
+    def forward(self, x):
+        pred = self.linear(x)
+        return pred
 
-metric = torchmetrics.Accuracy( task="multiclass", num_classes=5)
+model = LinearRegressionModel()
+print(model)
+print(list(model.parameters()))
 
-n_batches =10
-for i in range(n_batches):
-    preds = torch.randn(10, 5).softmax(dim=-1)
-    target = torch.randint(5, (10,))
+w,b = model.parameters()
+w1,b1 = w[0][0].item() , b[0].item()
+x1 = np.array([-30,30])
+y1 = w1*x1+b1
 
-    acc = metric(preds, target)
-    print(acc)
+plt.plot(x1,y1,'r')
+plt.scatter(x,y)
+plt.grid()
+plt.show()
 
-ac=metric.compute()
-print('\n',acc)
+#손실함수 설정
+criterion = nn.MSELoss()
+#옵티마이저 정의
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+#학습
+epochs = 100
+losses = []
+
+for epoch in range(epochs):
+    optimizer.zero_grad()
+
+    y_pred = model(x)
+    loss = criterion(y_pred, y)
+    losses.append(loss.item())
+    loss.backward()
+
+    optimizer.step()
+
+plt.plot(range(epochs),losses)
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.show()
+
